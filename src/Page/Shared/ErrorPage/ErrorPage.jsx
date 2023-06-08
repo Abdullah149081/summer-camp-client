bb1688a65615d160ec1669b3db722a80f43f14b6
Abdullah149081 <masudmd425@gmail.com>
2023-06-08 20:19:03 +0600

    error page is done

--- src/Page/Shared/ErrorPage/ErrorPage.jsx
+++ src/Page/Shared/ErrorPage/ErrorPage.jsx
@@ -7,11 +7,13 @@ const ErrorPage = () => {
     <div>
       <div className="flex items-center justify-center flex-col min-h-screen">
         <img className="lg:w-1/4 " src="https://i.ibb.co/hKxDPBs/7938322-3814348.jpg" alt="" />
-        <p className=" text-red-500 font-medium text-2xl">
+        <p className=" text-error font-medium text-2xl ">
           <i>{error?.statusText || error?.message}</i>
         </p>
         <Link to="/">
-          <button type="button" className="btn btn-secondary">back to home</button>
+          <button type="button" className="mt-6 btn-camp">
+            back to home
+          </button>
         </Link>
       </div>
     </div>
