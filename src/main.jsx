cfeecfcd91d367f50598ed24a0a6661be0710a03
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 22:42:34 +0600

    useAxiosSecure hooks create

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
 import AuthProviders from "./Providers/AuthProviders";
 import router from "./Routes/Routes";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <AuthProviders>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </AuthProviders>
     </HelmetProvider>
   </React.StrictMode>
