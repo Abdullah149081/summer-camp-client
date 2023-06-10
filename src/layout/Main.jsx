f362a5640d400863864eddc6d365d8cdcb721c92
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 18:45:25 +0600

    Main layout loading add

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-absolute-path */
-
-import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
-
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
 import Footer from "../Page/Shared/Footer/Footer";
 import Header from "../Page/Shared/NavBar/Header";
+import logo from "/logo.png";
 
 const Main = () => {
+  const { loading } = useAuth();
+  if (loading) {
+    return (
+      <div className="flex flex-col min-h-screen justify-center mt-4 items-center">
+        <div>
+          <img className="w-60" src={logo} alt="" />
+        </div>
+        <h2 className="text-4xl font-bold mt-4  animate-pulse">SportsRookieCamp</h2>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
