0de0d8a8fbdb4660deee4a5f453dc1c8b916aff4
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 22:53:44 +0600

    useAdmin hooks create

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import AuthProviders from "./Providers/AuthProviders";
 import router from "./Routes/Routes";
