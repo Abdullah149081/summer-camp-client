0821c8cceaf29132a8be8072adc6c386e474a564
Abdullah149081 <masudmd425@gmail.com>
2023-06-12 21:31:42 +0600

    selected api is done

---
+++ src/Hooks/useClass.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useClass = () => {
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const { refetch, data: join = [] } = useQuery({
+    queryKey: ["classes", user?.email],
+    enabled: !loading && !!user?.email && !!localStorage.getItem("token"),
+    queryFn: async () => {
+      const res = await axiosSecure(`/carts?email=${user?.email}`);
+      return res.data;
+    },
+  });
+  return [join, refetch];
+};
+
+export default useClass;
